96e736d605553a20eaa86e0e2c57ebef68b4a847
Divyansh7agl <divyansh.agl.jpr@gmail.com>
2024-10-21 23:55:03 +0530

    Add files via upload

---
+++ AIRINDEX.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int aqi;
+    cin>>aqi;
+    
+    if (aqi<100){
+    cout<<"YES";
+    }
+    else{
+    cout<<"NO";
+    }
+	return 0;
+
+}
