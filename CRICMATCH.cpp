44dbcc6618d4cee37e149cb24965d2ae0fdc9338
Divyansh7agl <divyansh.agl.jpr@gmail.com>
2024-10-25 09:13:57 +0530

    Add files via upload

---
+++ CRICMATCH.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n; int m;
+	    cin >> n >>m;
+	  int u= 36*m;
+	   if (u>=n){
+	       cout<<"yes"<<endl;
+	   
+	   }
+	   else {
+	       cout<<"no"<<endl;
+	   }
+	}
+return 0;
+}
